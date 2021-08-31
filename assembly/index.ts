6b808a62bd8854762a0b5868566fc1a86b27fb84
Hieu Pham <hieu.phamnt39@gmail.com>
2021-08-31 23:04:51 +0700

    update updateMatch function

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,6 +1,6 @@
 import * as match from "./match";
 import * as user from "./user";
-import { Match, MatchMode } from "./model";
+import { Match, MatchMode, MatchState } from "./model";
 import { User } from "./model";
 
 export function createMatch(mode: MatchMode): String {
@@ -13,4 +13,8 @@ export function createUser(alias: string, bio: string, avatar: string): User {
 
 export function getMatch(): Match[] {
     return match.getMatch()
+}
+
+export function updateMatch(id: string, state: MatchState): bool {
+    return match.updateMatch(id, state);
 }
--- assembly/match.ts
+++ assembly/match.ts
@@ -1,4 +1,11 @@
-import { Context, PersistentMap, PersistentVector, math, PersistentSet, logging } from "near-sdk-as";
+import {
+  Context,
+  PersistentMap,
+  PersistentVector,
+  math,
+  PersistentSet,
+  logging,
+} from "near-sdk-as";
 import { Match, MatchMode, MatchState } from "./model";
 
 let waitingMatch = new PersistentVector<Match>("w");
@@ -20,20 +27,25 @@ let idGenerated = new PersistentSet<String>("i");
  * id of match
  */
 export function createMatch(mode: MatchMode): String {
-    logging.log("creating match");
-    let matchId: String = "2";
-    // while (matchId == "") {
-    //     let idTmp: String = _makeid(32);
-    //     if (!idGenerated.has(idTmp)) {
-    //         matchId = idTmp;
-    //         idGenerated.add(idTmp);
-    //     }
-    // }
+  logging.log("creating match");
+  let matchId: String = "";
+  while (matchId == "") {
+      let idTmp: String = _makeid(32);
+      if (!idGenerated.has(idTmp)) {
+          matchId = idTmp;
+          idGenerated.add(idTmp);
+      }
+  }
 
-    logging.log("createMatch from: " + Context.sender + " bet: " + Context.attachedDeposit.toString());
+  logging.log(
+    "createMatch from: " +
+      Context.sender +
+      " bet: " +
+      Context.attachedDeposit.toString()
+  );
 
-    waitingMatch.push(new Match(matchId, Context.attachedDeposit, mode));
-    return matchId;
+  waitingMatch.push(new Match(matchId, Context.attachedDeposit, mode));
+  return matchId;
 }
 
 /**
@@ -41,23 +53,64 @@ export function createMatch(mode: MatchMode): String {
  */
 
 export function getMatch(): Match[] {
-    const waitingMatchLength = waitingMatch.length;
-    let results = new Array<Match>(waitingMatchLength);
-    for (let i = 0; i < waitingMatchLength; i++) {
-        results[i] = waitingMatch[i];
-    }
-    return results;
+  const waitingMatchLength = waitingMatch.length;
+  let results = new Array<Match>(waitingMatchLength);
+  for (let i = 0; i < waitingMatchLength; i++) {
+    results[i] = waitingMatch[i];
+  }
+  return results;
 }
 
 /**
  * Private function
  */
 function _makeid(length: i32): String {
-    var result: String = "";
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(<i32>Math.floor(<i32>Math.random() * charactersLength));
+  var result: String = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(
+      <i32>Math.floor(<i32>Math.random() * charactersLength)
+    );
+  }
+  return result;
+}
+
+export function updateMatch(id: string, state: MatchState): bool {
+  logging.log("Updating match: " + id + "State: " + state.toString());
+  const finishedMatchLength = finishedMatch.length;
+  const runningMatchLength = runningMatch.length;
+  let matchIndex: i32;
+  switch (state) {
+    case MatchState.RUNNING:
+        matchIndex = getMatchId(id, waitingMatch);
+        if (matchIndex > -1) {
+            runningMatch.push(waitingMatch[matchIndex]);
+            logging.log("waitingMatch[matchIndex]" + waitingMatch[matchIndex].id);
+            waitingMatch.swap_remove(matchIndex);
+        }
+        break;
+    case MatchState.FINISHED:
+        matchIndex = getMatchId(id, runningMatch);
+        if (matchIndex > -1) {
+            finishedMatch.push(runningMatch[matchIndex]);
+            logging.log("runningMatch[matchIndex]" + runningMatch[matchIndex].id)
+            runningMatch.swap_remove(matchIndex);
+        }
+        break;
+    default: 
+        break;
+  }
+  let result: bool = true;
+  return result;
+}
+
+function getMatchId(id: string, matchVector: PersistentVector<Match>): i32 {
+  for (let i: i32 = 0; i < matchVector.length; i++) {
+    if (matchVector[i].id === id) {
+      return i;
     }
-    return result;
+  }
+  return -1;
 }
